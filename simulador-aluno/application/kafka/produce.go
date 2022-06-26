e172d21828c84a6c6615d825a28aceed6555be6b
Ygor Alves <ybarbozalves@gmail.com>
2022-06-26 13:35:48 -0300

    init of simulator

---
+++ simulador-aluno/application/kafka/produce.go
@@ -0,0 +1,28 @@
+package kafka
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/ygoralves/imersaofsfc2-simulator/infra/kafka"
+	route2 "github.com/ygoralves/imersaofsfc2-simulator/application/route"
+	"log"
+	"os"
+	"encoding/json"
+	"time"
+)
+
+//{"clientId":"1", "routeId": "1"}
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route2.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Millisecond * 500)
+	}
+
+
+}
